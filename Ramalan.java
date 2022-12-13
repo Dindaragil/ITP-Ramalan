eb9ecdaa7f6c10adf80b6a4cadef210969c2adb2
Dinda Ragil <dindawardani36@gmail.com>
2022-12-13 18:15:28 +0700

    fix: input

--- Ramalan.java
+++ Ramalan.java
@@ -14,13 +14,15 @@ public class Ramalan {
         name = input.nextLine();
         System.out.printf("Masukkan umur Anda: ");
         age = input.nextInt();
+        input.nextLine();
 
-        // b. Minta user untuk menginput nama pasangan dan umur pasangan. 
+        // b. Minta user untuk menginput nama pasangan dan umur pasangan.
         System.out.printf("Data pasangan Anda: \n");
         System.out.printf("Masukkan nama pasangan Anda: ");
         partnerName = input.nextLine();
         System.out.printf("Masukkan umur pasangan Anda: ");
         partnerAge = input.nextInt();
+        input.nextLine();
 
         System.out.printf("\n\n");
 
@@ -31,12 +33,12 @@ public class Ramalan {
         System.out.println(name + " [" + age + "] " + "tahun");
         System.out.println(partnerName + " [" + partnerAge + "] " + "tahun");
         System.out.println("Tekan ENTER untuk melihat hasil ramalan...");
-        String enter = input.nextLine();
+        input.nextLine();
 
         //menentukan angka acak untuk kecocokan
         int min = 50;
         int max = 100;
-              
+
         //Generate random angka dari 50 ke 100
         double doubleRandom = (double)Math.floor(Math.random()*(max-min+1)+min);
         System.out.printf("Kecocokan anda dengan pasangan anda adalah: %.2f", doubleRandom );
